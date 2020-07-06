3e8074e5bff217acae861f7a834a5be48dba6000
Milana Maksimovic <milana.maksimovic1996@gmail.com>
2020-07-06 15:51:40 +0200

    Implemented search

--- src/components/Currencies.vue
+++ src/components/Currencies.vue
@@ -3,15 +3,17 @@
     <div class="sidenav-container">
       <div class="sidenav-container-nav">
         <p class="sidenav-container-nav-header header-orange">Currency list</p>
-
+        <Input v-model="search" />
         <Item
-          v-for="currency in currencies"
+          v-for="currency in filteredCurrencies"
           :key="currency.code"
           :currency="currency"
         >
-        
         </Item>
-        <router-link class="link add-currency orange" :to="{ path: '/currencies/add' }">
+        <router-link
+          class="link add-currency orange"
+          :to="{ path: '/currencies/add' }"
+        >
           <span><font-awesome-icon icon="plus-circle"/></span>
           Add currency</router-link
         >
@@ -23,32 +25,40 @@
 
 <script>
 import Item from "../components/currencies/Item";
+import Input from "../components/UI/Input";
 export default {
   name: "Currencies",
   components: {
-    Item
+    Item,
+    Input
   },
   data() {
     return {
       // currencies: []
+      search: ""
     };
+  },
+  computed: {
+    filteredCurrencies() {
+      const searchText = this.search.toLowerCase();
+      return this.$store.state.currencies.filter(el => {
+        console.log(el.id);
+        
+        return (
+          el.code.toLowerCase().includes(searchText) ||
+          el.symbol.toLowerCase().includes(searchText) ||
+          el.id == searchText
+        );
+      });
+    }
   },
   mounted() {
     // this.$store.dispatch('fetchCurrencies');
     // if(localStorage.getItem('currencies')) {
     //   this.currencies = JSON.parse(localStorage.getItem('currencies'));
     //   console.log(this.currencies);
-      
     // }
     // console.log(this.$store.state.currencies);
-    
-  },
-  computed: {
-    currencies() {
-      return this.$store.state.currencies;
-    }
-    
   }
 };
 </script>
-
